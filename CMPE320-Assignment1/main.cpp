ddf00b716dfc87bb41349a40cb1eda35f59e3920
Ethan Silver <ejrsilver@gmail.com>
2022-09-08 09:21:39 -0400

    Initial Commit

---
+++ CMPE320-Assignment1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CMPE320-Assignment1
+//
+//  Created by Ethan Silver on 2022-09-08.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
