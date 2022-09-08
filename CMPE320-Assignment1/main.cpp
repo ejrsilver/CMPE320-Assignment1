e89861494f5f50689449c9e8c625cea0513d3c6a
Ethan Silver <ejrsilver@gmail.com>
2022-09-08 13:16:46 -0400

    Created Initialize function, read/write still non-functional

--- CMPE320-Assignment1/main.cpp
+++ CMPE320-Assignment1/main.cpp
@@ -6,9 +6,38 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+
+string in1[50];
+string in2[50];
+string in3[50];
+
+void initialize() {
+  ifstream myFile;
+  myFile.open("./InsultsSource.txt");
+  string s;
+  
+  string delim = "\t";
+  int x = 0;
+  if(myFile.is_open()) {
+    while(getline(myFile, s)) {
+      in1[x] = s.substr(0, s.find(delim));
+      s.erase(0, s.find(delim) + delim.length());
+      in2[x] = s.substr(0, s.find(delim));
+      s.erase(0, s.find(delim) + delim.length());
+      in3[x] = s.substr(0, s.find(delim));
+      x++;
+    }
+  }
+  else {
+    throw system_error(error_code(), "File not found");
+  }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+  initialize();
 }
