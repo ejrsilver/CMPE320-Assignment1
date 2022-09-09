4d3dd6878bba8ba8937229c37b4df020e69bdd37
Ethan Silver <ejrsilver@gmail.com>
2022-09-09 13:43:58 -0400

    Added header

---
+++ insultgenerator_19es25.h
@@ -0,0 +1,17 @@
+//
+//  insultgenerator_19es25.h
+//  CMPE320-Assignment1
+//
+//  Created by Ethan Silver on 2022-09-09.
+//
+
+#ifndef insultgenerator_19es25_h
+#define insultgenerator_19es25_h
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+
+
+#endif /* insultgenerator_19es25_h */
