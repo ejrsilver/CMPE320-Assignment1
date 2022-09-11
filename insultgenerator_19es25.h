4ce16f26c2d3213dd3d22727afefbcc3cc1e97f4
Ethan Silver <ejrsilver@gmail.com>
2022-09-11 12:02:04 -0400

    Moved code to header.

--- insultgenerator_19es25.h
+++ insultgenerator_19es25.h
@@ -22,4 +22,54 @@ class FileException:public exception {
     }
 };
 
+class NumInsultsOutOfBounds:public exception {
+  public:
+    const char * what() const throw() {
+      return "File not found!";
+    }
+};
+
+class InsultGenerator {
+  
+  public:
+    const char * what() const throw() {
+      return "File not found!";
+    }
+private:
+  string in1[50];
+  string in2[50];
+  string in3[50];
+  long * generateRandom (long x0, long a, int b ,int m, int n) {
+    long *r[n];
+    // Linear congurential generator for n inputs
+    for(int i = 0;i < n; i++) {
+      x0 = (a * x0 + b) % m;
+      *r[i] = x0;
+    }
+    return r[n];
+  }
+
+  void initialize() {
+    ifstream myFile;
+    myFile.open("./InsultsSource.txt");
+    string s = "";
+    
+    string delim = "\t";
+    int x = 0;
+    if(myFile.is_open()) {
+      while(getline(myFile, s)) {
+        in1[x] = s.substr(0, s.find(delim));
+        s.erase(0, s.find(delim) + delim.length());
+        in2[x] = s.substr(0, s.find(delim));
+        s.erase(0, s.find(delim) + delim.length());
+        in3[x] = s.substr(0, s.find(delim));
+        x++;
+      }
+    }
+    else {
+      throw system_error(error_code(), "File not found");
+    }
+  }
+};
+
 #endif /* insultgenerator_19es25_h */
